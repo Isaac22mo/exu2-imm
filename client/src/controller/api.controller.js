e091c5b3c511dcd49a65e1b25597d84ccbcfd592
Isaac22mo <20223tn135@utez.edu.mx>
2025-12-11 22:30:10 -0600

    cambios de variable

--- client/src/controller/api.controller.js
+++ client/src/controller/api.controller.js
@@ -1,6 +1,6 @@
 const ApiController = {};
 const ENV = import.meta.env;
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
+const API_URL = `${ENV.VITE_API_url}`;
 
 ApiController.calculatorSuma = async suma => {
     try {
